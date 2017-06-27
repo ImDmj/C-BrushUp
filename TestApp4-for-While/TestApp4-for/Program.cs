edccf47a1ce1a0b88c1f755ebe305e7e5f05ac9d
ImDmj <mdhaninkula@unomaha.edu>
2017-06-27 18:52:51 -0500

    For, While, Functions Calls by Vaue and reference

---
+++ TestApp4-for-While/TestApp4-for/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp4_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the table of the number
+            int i=0;
+            Console.WriteLine("Enter the number");
+            int no1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nTable of {0}",no1);
+            //for (i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("{0}x{1}={2}",no1,i,(no1*i));
+            //}
+
+            while (i<=10)
+            {
+                Console.WriteLine("{0}x{1}={2}", no1, i, (no1 * i));
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestApp5-ValueReference/TestApp5-ValueReference/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp5_ValueReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no1 = 10;
+            int no2 = 20;
+            Console.WriteLine("Values before swap");
+            Console.WriteLine("no1 = {0}", no1);
+            Console.WriteLine("no1 = {0}", no2);
+
+            //no1,no2 - Actual Parameters
+            swap( ref no1,ref no2);
+
+            Console.WriteLine("Values after swap");
+            Console.WriteLine("no1 = {0}", no1);
+            Console.WriteLine("no1 = {0}", no2);
+
+            Console.ReadLine();
+        }
+
+        //a,b - Fomal Parameters
+        static void swap(ref int a,ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
+
+// Values will not be swapped as we are just passing the values, hence there will not be nay change to the actual 
+// parameters. So we use ref or out to use call by reference and chage the value of original parameters.
