afe1d1a0672119d0b3dd722118e6541ed7b318fe
ImDmj <mdhaninkula@unomaha.edu>
2017-06-27 18:59:57 -0500

    Getter and Setters

---
+++ TestApp8-ClassGetSet/TestApp8-ClassGetSet/Date.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp8_ClassGetSet
+{
+    class Date
+    {
+        private int day;
+        private int month;
+        private int year;
+
+        // properties 
+        public int Day
+        {
+            get { return day; }
+            set  // set ( int value) - implicit. value given by the user is passed as the parameter to the set()
+            {
+              if(value>=1 && value<=31)
+              {
+                  day = value;
+              }
+            else
+              {
+                  day = 1;
+              }
+            }
+        }
+        public int Month
+        {
+            get { return month; }
+            set
+            {
+                if( value>=1 && value<=12)
+                {
+                    month = value;
+                }
+                else
+                {
+                    month = 1;
+                }
+            }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set{ year = value;}
+        }
+
+        public string printdate()
+        {
+            return day + "/" + month + "/" + year;
+        }
+    }
+}
---
+++ TestApp8-ClassGetSet/TestApp8-ClassGetSet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp8_ClassGetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date dte = new Date();
+            Console.WriteLine("\nEnter the day");
+            dte.Day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the month");
+            dte.Month =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the year");
+            dte.Year =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(dte.printdate());
+
+            Console.ReadLine();
+        }
+    }
+}
