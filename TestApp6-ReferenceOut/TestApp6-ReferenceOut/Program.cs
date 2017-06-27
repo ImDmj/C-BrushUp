d4893d2fca362cdf6369e4b997b7dd0c6be633ab
ImDmj <mdhaninkula@unomaha.edu>
2017-06-27 18:57:01 -0500

    Out Keyword

---
+++ TestApp6-ReferenceOut/TestApp6-ReferenceOut/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp6_ReferenceOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 5;
+            double area, circum;
+
+            calculate(r, out area, out circum);
+
+            Console.WriteLine("Area = {0}",area);
+            Console.WriteLine("Circumference = {0}", circum);
+
+            Console.ReadLine();
+
+        }
+
+        static void calculate(int r, out double a, out double c)
+        {
+            a = 3.14 * r * r;
+            c = 2 * 3.14 * r;
+        }
+    }
+}
