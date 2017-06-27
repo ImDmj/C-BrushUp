1a607811c5fd0c25b82e64f60cbb1f1603cc3f6e
ImDmj <mdhaninkula@unomaha.edu>
2017-06-27 17:43:23 -0500

    Lets Get Started

---
+++ TestApp1/TestApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hi");
+            //string str = Console.ReadLine();
+            //Console.WriteLine("you entered {0}", str);
+            //
+            for (int x = 0; x < 10; x++)
+            {
+                 Console.WriteLine("{0}",x);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
