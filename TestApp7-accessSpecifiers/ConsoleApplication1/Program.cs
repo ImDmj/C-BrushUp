7c5f6e00016a70203396a0ee7b6536f4de8c0e35
ImDmj <mdhaninkula@unomaha.edu>
2017-06-27 18:58:53 -0500

    Access Specifiers

---
+++ TestApp7-accessSpecifiers/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date dt = new Date();
+            // this works only when the data members of the class are public 
+            // if they are private then we need to use the get and set methods
+            dt.day = 10;
+            dt.month = 3;
+            dt.year = 2017;
+
+            Console.WriteLine("{0}/{1}/{2}", dt.day, dt.month, dt.year);
+
+            Console.ReadLine();
+        }
+    }
+}
