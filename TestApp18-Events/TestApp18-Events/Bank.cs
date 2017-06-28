b61246be5d79401fb5d306197c07c28d3eb8dd82
ImDmj <mdhaninkula@unomaha.edu>
2017-06-27 19:14:29 -0500

    Events and Event Handling

---
+++ TestApp18-Events/TestApp18-Events/Bank.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp18_Events
+{
+    public delegate void DelLowBalance();
+
+    public class Bank
+    {
+        private int accno;
+        private string name;
+        private int balance;
+
+        //declare an event
+        public event DelLowBalance LowBalance;
+
+        public Bank()
+        {
+            accno = 1;
+            name = "Sam";
+            balance = 50000;
+        }
+
+        public Bank(int a, string n, int b)
+        {
+            accno = a;
+            name = n;
+            balance = b;
+        }
+
+        public void deposit(int amt)
+        {
+            balance += amt;
+        }
+
+        public void WithDraw(int amt)
+        {
+            if (balance-amt>=5000)
+            {
+                balance -= amt;
+            }
+            else
+            {
+                //notify i.e, raise an event...
+                LowBalance();
+            }
+        }
+
+        public override string ToString()
+        {
+            return accno+ " " + name + " " + balance;
+        }
+
+    }
+}
---
+++ TestApp18-Events/TestApp18-Events/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp18_Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b = new Bank(111, "Raj", 60000);
+            //Creating an object of the instance of the delegate..Attaching delegate to the event
+            b.LowBalance += new DelLowBalance(ProcessLowBalance);
+
+            Console.WriteLine(b.ToString());
+
+            b.deposit(5000);
+            Console.WriteLine(b.ToString());
+
+            b.WithDraw(62000);
+            Console.WriteLine(b.ToString());
+
+            Console.ReadLine();
+        }
+
+        public static void ProcessLowBalance()
+        {
+            Console.WriteLine("Min Balance not maintained. Transaction Failed...");
+        }
+    }
+}
