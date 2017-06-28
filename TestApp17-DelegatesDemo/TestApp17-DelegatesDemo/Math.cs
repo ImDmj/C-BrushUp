1a352c425f2c69d45e48ac1e85d78d0c3a5cf58f
ImDmj <mdhaninkula@unomaha.edu>
2017-06-27 19:13:28 -0500

    Something about Delegates

---
+++ TestApp17-DelegatesDemo/TestApp17-DelegatesDemo/Math.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp17_DelegatesDemo
+{
+    public class Math
+    {
+
+        public void add(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+
+        public void sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        public void multi(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        public string UpperCase(string s)
+        {
+            Console.WriteLine("Upper Case");
+            return s.ToUpper();
+        }
+
+        public string LowerCase(string s)
+        {
+            Console.WriteLine("Lower Case");
+            return s.ToLower();
+        }
+    }
+}
---
+++ TestApp17-DelegatesDemo/TestApp17-DelegatesDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp17_DelegatesDemo
+{
+    public delegate void mydelegate(int a, int b);
+    public delegate string mystringdelegate(string s);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math m = new Math();
+            Console.Clear();
+            //Instantiating Delegate
+            //Mydelegate d = new MydelegateAdd(m.add);
+            //d += m.multi; // multicast delegates
+            //d += m.sub;
+            ////Invoking Delegates in two ways
+            //d(10,20);
+            //d.Invoke(10,20);
+
+            mystringdelegate str = new mystringdelegate(m.UpperCase);
+            //str += m.UpperCase;
+            str += m.LowerCase;
+            //Console.WriteLine(str("hElLo"));
+            // Only printing hello for the lowercase and not uppercase. In case of 
+            // multicast delegate, recommneded return is void, like we did for math functions.
+
+            Delegate[] del = str.GetInvocationList();
+            foreach (Delegate d in del)
+            {
+               Console.WriteLine((d as mystringdelegate).Invoke("hello"));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
