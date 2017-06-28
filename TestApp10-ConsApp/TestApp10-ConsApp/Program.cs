60d3bab286ecfdf26bb668150e59dde695f6b4b7
ImDmj <mdhaninkula@unomaha.edu>
2017-06-27 19:03:59 -0500

    use the class library from TestApp9

---
+++ TestApp10-ConsApp/TestApp10-ConsApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//When using Class library from somewhere then add it as header library/namespace of the class from the library
+using TestApp10_ClassLibrary;
+
+namespace TestApp10_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            Console.WriteLine("Enter the employee number");
+            emp.eno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the employee name");
+            emp.ename = Console.ReadLine();
+            Console.WriteLine("Enter the designation of the employee ");
+            emp.desig = Console.ReadLine();
+            Console.WriteLine("Enter the basic salary of the employee ");
+            emp.bsal = Convert.ToInt32(Console.ReadLine());
+            emp.calculate();
+            emp.printdetails();
+            Console.ReadLine();
+           
+        }
+    }
+}
